e884a165d3769f7228ca0e4ab05f22682ec61806
syakimov <stefan.yakimov@tradeo.com>
2018-02-09 07:44:15 +0200

    Small amendments on HW client

--- zmq/hw/client.go
+++ zmq/hw/client.go
@@ -1,15 +1,8 @@
-//
-//  Hello World client.
-//  Connects REQ socket to tcp://localhost:5555
-//  Sends "Hello" to server, expects "World" back
-//
-
 package main
 
 import (
-	zmq "github.com/pebbe/zmq3"
-
 	"fmt"
+	zmq "github.com/pebbe/zmq3"
 )
 
 func main() {
@@ -19,7 +12,7 @@ func main() {
 	defer requester.Close()
 	requester.Connect("tcp://localhost:5555")
 
-	for request_nbr := 0; request_nbr != 10; request_nbr++ {
+	for request_nbr := 0; request_nbr <= 3; request_nbr++ {
 		// send hello
 		msg := fmt.Sprintf("Hello %d", request_nbr)
 		fmt.Println("Sending ", msg)
