9cd1260ca709dd4ada2853c789fe8379e1865574
syakimov <stefan.yakimov@tradeo.com>
2018-05-24 14:40:58 +0300

    Not solved yet

---
+++ leetcode/ruby/3_sum.rb
@@ -0,0 +1,37 @@
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def three_sum(nums)
+  @comb
+end
+
+def combine(nums, index = 0, start, limit)
+  if index >= nums.size
+    p arr
+    return
+  end
+
+  i = start
+  while i <= limit
+    arr[index]
+  end
+end
+
+def sol(arr, start, temp)
+  i = start
+  while i <= 3
+    temp << arr[i]
+    sol(arr, i + 1, temp)
+    i += 1
+  end
+end
+
+# Given array nums = [-1, 0, 1, 2, -1, -4],
+
+# A solution set is:
+# [
+  # [-1, 0, 1],
+  # [-1, -1, 2]
+# ]
+
+# 0
+# [-1, 0, 2, 1],
---
+++ leetcode/ruby/phone_number_combination.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+def letter_combinations(digits)
+  ans = ['']
+  return ans if digits.size == 0
+  mapping = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
+end
+
+
